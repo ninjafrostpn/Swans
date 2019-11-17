3ea1154e53b4af82a9307eea97a1fdea9d727451
ninjafrostpn <cst1g16@soton.ac.uk>
2019-11-17 21:37:23 +0000

    Add site table extraction

--- WeBS Data Scraper/WeBS Scraper.py
+++ WeBS Data Scraper/WeBS Scraper.py
@@ -18,12 +18,12 @@ try:
     driver.get(muteswan_site)
     print(" - Waiting for table to load...")
     table = WebDriverWait(driver, 60).until(
-        EC.presence_of_all_elements_located((By.XPATH,'//table[@class="maintable"]'
-                                                      '/tbody'
-                                                      '/tr'
-                                                      '/td'
-                                                      '/div'
-                                                      '/*'))
+        EC.presence_of_all_elements_located((By.XPATH, '//table[@class="maintable"]'
+                                                       '/tbody'
+                                                       '/tr'
+                                                       '/td'
+                                                       '/div'
+                                                       '/*'))
     )
     print(" - Site and table loaded.\nExtracting data table...")
     # Joins table cells into a string, cells delineated using *
@@ -43,8 +43,8 @@ try:
 
     # Breaks the string up into cells again and formats them as a table with 10 cells per row
     table = np.object_(table.split("*"))
-    muteswa_table = table.reshape(-1, 10)
-    print(" - Data table extracted.\n\n", table, "\n")
+    muteswantable = table.reshape(-1, 10)
+    print(" - Data table extracted.\n\n", muteswantable, "\n")
 
     # Finds the location dropdown menu and clicks it
     print("Extracting names for sites with data available...")
@@ -61,6 +61,7 @@ try:
         if searchbox.is_displayed():
             # Clears it, just in case, then finds all the non-italicised dropdown elements
             # (The italicised ones don't appear to have any data on their tables)
+            locationsearchbox = searchbox
             searchbox.clear()
             sitenames = driver.find_elements_by_xpath('//li[contains(@class, "select2-result") and '
                                                       'not(contains(@class, "italicSpecies"))]'
@@ -72,30 +73,66 @@ try:
             sitenames = ["".join(re.split(r"<span.*span>", sitename.get_attribute("innerHTML")))
                          for sitename in sitenames]
             print(" - Site names extracted.\n\n", sitenames, "\n")
-    locationsearchbox = searchbox
 
     # Now for the real data extraction. This will enter each site name in turn and get the relevant data table
+    print("Extracting individual site data tables...")
+    sitetables = []
     for sitename in sitenames:
+        print(" - Extracting data table for " + sitename + "...")
+        print(" -  - Navigating to page...")
         locationsearchbox.send_keys(sitename + "\n")
+        time.sleep(2)
 
         # Ensure that the bird group selector is set to wildfowl
         # (The letter combination "wi" seems to consistently get it to select "wildfowl"; longer and shorter do not)
+        print(" -  - Selecting correct table settings...\n -  -  - Selecting wildfowl...")
         birdgroupdropdown = driver.find_element_by_xpath('//select[@id="birdgroup"]')
         birdgroupdropdown.send_keys("wi")
+        time.sleep(2)
 
         # Ensure that supplementary data are always included
+        print(" -  -  - Selecting supplementary data...")
         suppbutton = driver.find_element_by_xpath('//input[@id="supps"]')
         if not suppbutton.is_selected():
             suppbutton.click()
+        time.sleep(2)
 
         # Ensure that the birds are arranged in descending 5-year mean abundance
+        print(" -  -  - Selecting sort descending by 5-year-mean abundance...")
         mean5descendingbutton = driver.find_element_by_xpath('//th[@id="mean5"]'
                                                              '/span[@class="hal"]'
-                                                             '/span[contains(@class, "toggledown")]')
+                                                             '/span[contains(@class, "toggleDown")]')
         if "activeSort" not in mean5descendingbutton.get_attribute("class"):
             mean5descendingbutton.click()
+        time.sleep(2)
+        print(" -  -  - Settings set.")
 
-        # TODO: Extract table of data
+        table = driver.find_elements_by_xpath('//table[@class="maintable"]'
+                                              '/tbody'
+                                              '/tr'
+                                              '/td'
+                                              '/div'
+                                              '/*')
+
+        # Joins table cells into a string, cells delineated using *
+        # (Skips delineator which sits before the first cell)
+        table = "".join([t.text if t.text != "" else "*" for t in table[1:]])
+
+        # Replaces gaps left by supplementary data hiders with placeholders
+        table = " ({} [{}])*".join(table.split("**"))
+
+        # Gets the supplementary data sources and inserts them
+        hiders = driver.find_elements_by_xpath('//div[@class="hider"]'
+                                               '/*')
+        hiders = [t.get_attribute("innerHTML") for t in hiders]
+        hiders = re.split(r"</?em>", "".join(hiders))
+        print(hiders)
+        table = table.format(*hiders)
+
+        # Breaks the string up into cells again and formats them as a table with 10 cells per row
+        table = np.object_(table.split("*"))
+        sitetables.append(table.reshape(-1, 10))
+        print(" -  - Data table extracted.\n\n", sitetables[-1], "\n")
         break
 finally:
     print("Closing driver...")
